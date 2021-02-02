8ca84d7bd738fa283afc1c1c34dda705b2f0904f
Magno Matos <magnomatos822@gmail.com>
2021-02-02 20:36:17 -0300

    Criaçao class Category

--- myApp/models.py
+++ myApp/models.py
@@ -1,11 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields.related import ForeignKey
+
+class Category(models.Model):
+    """Model definition for Category."""
+
+    categorie = models.CharField(max_length = 150)
+    description = models.TextField(null=True, blank=True)
+    
+    
+
+    class Meta:
+        """Meta definition for Category."""
+
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        """Unicode representation of Category."""
+        return self.categorie
+
 
 class Post(models.Model):
     """Model definition for Post."""
 
     title = models.CharField(max_length = 150)
-    fieldName = models.CharField(max_length = 150, blank=True, null=True)
+    subtitle = models.CharField(max_length = 150, blank=True, null=True)
     comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='Autor')
+    category = models.ManyToManyField(Category, verbose_name=("Categoria"))
+    
+    
     
 
     class Meta:
@@ -14,3 +39,5 @@ class Post(models.Model):
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
 
+    def __str__(self):
+        return self.title
